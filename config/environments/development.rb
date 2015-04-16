7b8834272efc9157b837f33c480733f6565cc337
zhee <zcheeee@gmail.com>
2015-04-17 07:32:39 +0900

    Add log setting for Unicorn

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,10 +4,10 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes                     = false
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load                        = false
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
@@ -17,25 +17,51 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+  config.active_support.deprecation        = :log
 
   # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
+  config.active_record.migration_error     = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug                      = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.assets.digest = true
+  config.assets.digest                     = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
-  config.assets.raise_runtime_errors = true
+  config.assets.raise_runtime_errors       = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Unicorn log
+  config.logger                            = Logger.new('/dev/null')
+
+  # http://ruby-rails.hatenadiary.com/entry/20150204/1423055537
+  config.after_initialize do
+    Bullet.enable = true
+
+    Bullet.alert         = true
+    Bullet.bullet_logger = true
+    Bullet.console       = true
+    # Bullet.growl   = true      # Growl
+    # Bullet.xmpp = { :account  => 'bullets_account@jabber.org',
+    #                 :password => 'bullets_password_for_jabber',
+    #                 :receiver => 'your_account@jabber.org',
+    #                 :show_online_status => true }
+    # Bullet.rails_logger = true
+    # Bullet.bugsnag      = true
+    # Bullet.airbrake     = true
+    Bullet.raise         = true
+    # Bullet.add_footer   = true
+  end
+
+  config.react.variant        = :development
+  config.react.replay_console = true
+
 end
